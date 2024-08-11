23314fab36c5772c7a068e1a84552584ddf52938
balkirat-chahal <balkirat.chahal@gmail.com>
2024-08-11 13:27:20 -0600

    Added recoil state management library, created an atom for userState and fixed light/dark theme appreance in RightSection

--- src/Nearby.js
+++ src/Nearby.js
@@ -3,10 +3,10 @@ import { Avatar } from '@mui/material'
 
 function Nearby(props) {
   return (
-    <div className='flex items-center mb-3 justify-around'>
+    <div className='flex items-center mb-3 ml-2 justify-around'>
     <div className='flex items-center'>
       <Avatar className='mr-2' src={props.avatar}/>
-      <p className='text-white'>Random</p>
+      <p className='text-black dark:text-white'>Random</p>
       </div>
       <button className='flex rounded-sm m-1'>
         <p className='text-xl text-yellow-300'>Dab</p>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
 );
