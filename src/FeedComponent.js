1e797dc5f01d8ad50bbceb9a739f33d3cab4119e
Brar's Mac <brarsmac@Brarss-MacBook.local>
2024-08-22 19:39:51 -0600

    added like function

--- src/FeedComponent.js
+++ src/FeedComponent.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import sampleImg from './assets/sampleImg.png';
 import { Avatar } from '@mui/material';
 import LikeIcon from '@mui/icons-material/FavoriteBorder';
+import LikedIcon from '@mui/icons-material/Favorite';
 import CommentIcon from '@mui/icons-material/Comment';
 import SendIcon from '@mui/icons-material/Send';
 
-
 function FeedComponent() {
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleLikeClick = () => {
+    if (liked) {
+      setLikeCount(likeCount - 1);
+    } else {
+      setLikeCount(likeCount + 1);
+    }
+    setLiked(!liked);
+  };
+
   return (
     <div className='
     ml-2
@@ -24,7 +36,7 @@ function FeedComponent() {
             <Avatar className='m-2'/>
             <h2 className='text-black dark:text-white'>User</h2>
         </div>
-      <img src={ sampleImg } className='
+      <img src={sampleImg} className='
       max-w-sm
       sm:max-w-lg
       ml-auto
@@ -32,12 +44,15 @@ function FeedComponent() {
       '/>
 
       <div className='p-4'>
-        <button><LikeIcon className='m-2 mr-2 text-black dark:text-white lg'/></button>
-        <button><CommentIcon className='m-2 mr-2 text-black dark:text-white lg'/></button>
-        <button><SendIcon className='m-2 mr-2 text-black dark:text-white lg'/></button>
+        <button onClick={handleLikeClick}>
+          {liked ? <LikedIcon className='m-2 mr-2 text-red-500 lg' /> : <LikeIcon className='m-2 mr-2 text-black dark:text-white lg' />}
+        </button>
+        <span className='text-black dark:text-white'>{likeCount}</span>
+        <button><CommentIcon className='m-2 mr-2 text-black dark:text-white lg' /></button>
+        <button><SendIcon className='m-2 mr-2 text-black dark:text-white lg' /></button>
       </div>
     </div>
   )
 }
 
-export default FeedComponent
+export default FeedComponent;
